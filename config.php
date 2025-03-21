a007f61a114cccff76444696178a9354caab5236
Rodrigo Souza <srodrigo.souza025@gmail.com>
2025-03-21 17:28:46 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,41 @@
+<?php 
+/*
+||||================================================||||
+||||================================================||||
+||||    	Speed Test RS By Rodrigo Souza          ||||
+||||================================================||||
+||||================================================||||
+*/
+
+$site_name = 'Speed Test RS';								/* Website Title here */
+$site_descr = 'Verifique a velocidade da sua conexão de Internet';	/* Website description here */
+$copyrights = 'Rodrigo Souza © 2025';							/* footer Copyrights */
+$website_url = 'https://www.speedtestrs.com.br';		                /* Website Url*/
+
+$adsense_validation = 'ca-pub-XXXXXXXXX';				        /* google adsense validation ID */
+$analytics_tracking = 'UA-XXXXXXXXX';							/* google analytics ID */
+
+
+/***   data ad client small-top   ***/
+$small_ads_id ='
+
+<!-- Ad Unit Code Here --!>
+
+';
+
+
+/***   data ad client big-bottom    ***/
+$big_ads_id = '
+
+<!-- Ad Unit Code Here --!>
+
+';
+
+/*
+||||================================================||||
+||||================================================||||
+||||   	 Speed Test RS By Rodrigo Souza          	||||
+||||================================================||||
+||||================================================||||
+*/
+?>
---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+
+<footer>
+		<div class="footer">
+		<p>
+			<?php echo "$copyrights" ;?>
+		</p>
+	</div>
+
+</footer>	
